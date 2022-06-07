70766f1a16e844717c4da1f17394993456377017
Paul Koos <paully_k5298@yahoo.com>
2022-06-07 14:23:25 -0700

    normalize all list comprehensions to use curly brackets instead of do/end.

--- date/lib/main.rb
+++ date/lib/main.rb
@@ -9,9 +9,6 @@ class Farm
 
   def initialize
     # Plots have a name and some plants
-		plot_a = [["Sugar Baby", 3], ["Red Boomer", 1], ["Plump Pucker", 1]]
-		plot_b = [["Green Lady", 2], ["Dark Plump", 2], ["Rainbow", 1], ["Deep Darkness", 1]]
-    
 		plot_a = Plot.new('Plot A', [Plant.new('Sugar Baby', 3), Plant.new('Red Boomer', 1), Plant.new('Plump Pucker', 1)])
 		plot_b = Plot.new('Plot B', [Plant.new('Green Lady', 2), Plant.new('Dark Plump', 2), Plant.new('Rainbow', 1), Plant.new('Deep Darkness', 1)])
 		@plots = [plot_a, plot_b]
@@ -23,20 +20,13 @@ class Farm
 
   def total_number_of_plants
 		total = 0
-                  # Subtract one from size because name is not a plant
     plots.each { |plot| total += (plot.total_number_of_plants) }
 		total
   end
 
   def total_plant_heights
     total = 0
-    plots.each do |plot|
-			total += plot.total_plant_heights
-      # Remove the plot name before iterating over the plants.
-      # plot.each do |plant|
-      #   total += plant[1]
-      # end
-    end
+    plots.each { |plot| total += plot.total_plant_heights }
     total
   end
 
@@ -58,15 +48,12 @@ class Plot
 	end
 
 	def total_number_of_plants
-		# Subtract one from size because name is not a plant
 		plants.size
 	end
 
 	def total_plant_heights
 		total = 0
-		plants.each do |plant|
-			total += plant.height
-		end
+		plants.each { |plant| total += plant.height }
 		total
 	end
 end
