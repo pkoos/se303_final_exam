621613b931c2ec6f84b25881642ac454b8ad909c
Paul Koos <paully_k5298@yahoo.com>
2022-06-07 10:50:50 -0700

    rename first variable to verify function is... functioning.

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -2,7 +2,7 @@
 # To cure it, apply "extract function".
 
 def story
-  first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
+  first1234 = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
   middle = ""
   middle << "\n"
   middle << "\n"
